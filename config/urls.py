163c2b59cf070db3bab7a7e334ff3b914c18a7cc
Dmitry <dimakondykov@gmail.com>
2026-06-30 21:00:18 +0300

    32.2 Документирование и безопасность

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
@@ -14,7 +15,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
