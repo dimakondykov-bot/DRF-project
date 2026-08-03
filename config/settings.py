19e148d190bb90046e428514ad262a7a90bd66ee
Dmitry <dimakondykov@gmail.com>
2026-08-03 17:13:58 +0300

    django: update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.204.163']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
